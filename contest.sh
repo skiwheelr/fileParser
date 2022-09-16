efde345efb60805000d845e74a911e43205b5055
mawagner <mawagner@twilio.com>
2022-09-16 15:08:12 -0700

    jq working

--- contest.sh
+++ contest.sh
@@ -1,10 +1,13 @@
 #started 1:05 PM Friday sept 16
 #paused work at 1:23 PM
-TARGET = "https://api.instrumental.ai/healthcheck"
-# ping -c 1 $TARGET
-# echo $?
+#resumed work at 2:58 PM
 
+TARGET="https://api.instrumental.ai/healthcheck"
+# ping -c 1 $TARGET; echo $?
 # awk \{system("curl "$TARGET)}' /AbsPath/Input.txt' | jq
+# wget $TARGET | grep "Failed"
 
-wget $TARGET | grep "Failed"
-http://www.compciv.org/recipes/cli/jq-for-parsing-json/
+ELASTIC=$(curl -s ${TARGET} | jq '.elasticReachable')
+DB=$(curl -s ${TARGET} | jq '.databaseReachable')
+echo "Elastic service is available: "$ELASTIC
+echo "Database is available: "$DB
