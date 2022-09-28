4e1796256cf8dd9b6ad53c66c69fdb4921f7668d
mawagner <mawagner@twilio.com>
2022-09-28 09:50:20 +0200

    fixes

--- contest.sh
+++ contest.sh
@@ -3,14 +3,26 @@
 # resumed work at 2:58 PM
 # completed at 3:08 PM
 
-TARGET="https://api.instrumental.ai/healthcheck"
+TARGET="api.instrumental.ai"
+TARGET2="https://api.instrumental.ai/healthcheck"
 ping -c 1 ${TARGET} > /dev/null && echo "HOST FOUND" || echo "HOST NOT FOUND"
 
-ELASTIC=$(curl -s ${TARGET} | jq '.elasticReachable')
-DB=$(curl -s ${TARGET} | jq '.databaseReachable')
-echo "Elastic service is available: "$ELASTIC
-echo "Database is available: "$DB
+ELASTIC=$(curl -s ${TARGET2} | jq '.elasticReachable')
+if [ "$ELASTIC" = "true" ]
+then
+    echo '\033[0;32m'  "Elastic service is available: "$ELASTIC
+else
+    echo '\033[0;31m' "Elastic service is available: "$ELASTIC
+fi
 
+DB=$(curl -s ${TARGET2} | jq '.databaseReachable')
+
+if [ "$DB" = "true" ]
+then
+     echo '\033[0;32m' "Database is available: "$DB
+else
+    echo "Endpoint not found"
+fi
 
 # ping -c 1 $TARGET; echo $?
 # awk \{system("curl "$TARGET)}' /AbsPath/Input.txt' | jq
